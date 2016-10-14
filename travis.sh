fee1516dd2b00fff7e72f5b1a7b1bc15cb26655a
Ludovic Mussier <ludovic.mussier@non.schneider-electric.com>
2016-10-14 10:56:47 +0200

    Try to point to the correct folder

--- travis.sh
+++ travis.sh
@@ -5,5 +5,5 @@ env | sort
 mkdir ../build || true
 mkdir ../build/$CMAKE_BUILD_TYPE || true
 cd ../build/$CMAKE_BUILD_TYPE
-cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE ../superbuild
+cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE ../../superbuild
 make
