8082e51564a5c102aaea75b01d49dba26a7addca
Ludovic Mussier <ludovic.mussier@non.schneider-electric.com>
2016-10-14 10:48:22 +0200

    Set the build folder outside sources

--- travis.sh
+++ travis.sh
@@ -2,10 +2,8 @@
 set -evx
 env | sort
 
-pwd
-ls
-mkdir build || true
-mkdir build/$CMAKE_BUILD_TYPE || true
-cd build/$CMAKE_BUILD_TYPE
-cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE ../../superbuild
+mkdir ../build || true
+mkdir ../build/$CMAKE_BUILD_TYPE || true
+cd ../build/$CMAKE_BUILD_TYPE
+cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE ../superbuild
 make
